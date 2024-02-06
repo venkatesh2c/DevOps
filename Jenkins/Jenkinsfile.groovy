fb33784342c64b4f5fdd84481accdf4c134ac7d1
venkatesh2c <151626550+venkatesh2c@users.noreply.github.com>
2024-02-06 15:29:36 +0530

    Updated Jenkinsfile.groovy

--- Jenkins/Jenkinsfile.groovy
+++ Jenkins/Jenkinsfile.groovy
@@ -16,7 +16,7 @@ pipeline {
         stage('Stage-3'){
             steps{
                 sh '''
-                bash ${WORKSPACE}/Linux/Script.sh
+                bash ${WORKSPACE}/Linux/script.sh
                 '''
             }
         }
