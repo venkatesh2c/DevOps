9bd3fe4fe7a2935501331917c61f2617b1d2452b
venkatesh2c <151626550+venkatesh2c@users.noreply.github.com>
2024-02-06 15:36:10 +0530

    Updated Jenkinsfile.groovy

--- Jenkins/Jenkinsfile.groovy
+++ Jenkins/Jenkinsfile.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages{
         stage('Stage-1'){
             steps{
-                sh 'echo This is Stage-1'
+                script 'echo This is Stage-1 triggerd by ${User}'
                 }
         }
         stage('Stage-2'){
